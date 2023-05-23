250ce72d50d14ed09c9b0e4807db08aebd5fb040
Anatoli <anatoli@anatoli.home>
2023-05-23 19:11:20 +0300

    House page Partilly Complete

--- Views/houses.php
+++ Views/houses.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="en">
-<?php $page = 'Home'; ?>
+<?php $page = $_GET['page'] ?>
 <?php include('../Partial/landing/head.php'); ?>
 
 <body>
@@ -17,9 +17,9 @@
             <div class="site-navigation">
                 <a href="#" class="logo">House Hunt</a>
                 <ul class="js-clone-nav d-none d-lg-inline-block text-left site-menu">
-                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="index">Home</a></li>
                     <li><a href="about?page=About Us">About Us</a></li>
-                    <li><a href="houses?page=Houses">Houses</a></li>
+                    <li class="active"><a href="houses?page=Houses">Houses</a></li>
                     <li><a href="contact?page=Contact Us">Contact Us</a></li>
                 </ul>
                 <ul class="js-clone-nav d-none d-lg-inline-block text-left site-menu float-right menu-absolute">
@@ -35,9 +35,11 @@
         <div class="container">
             <div class="row align-items-center justify-content-between">
                 <div class="col-lg-5 intro">
-                    <h1 class="text-white font-weight-bold mb-4" data-aos="fade-up" data-aos-delay="0">Detective-Inspired House Hunt!</h1>
-                    <div id="typewriter"></div>
-                    <a href="" class="btn btn-primary" data-aos="fade-up" data-aos-delay="200"> Get Started</a>
+                    <h1 class="text-white font-weight-bold mb-4" data-aos="fade-up" data-aos-delay="0">Houses </h1>
+                    <p class="text-white mb-4" id="headline" data-aos="fade-up" data-aos-delay="100">Welcome to the Houses page on House Hunt, your gateway to discovering the perfect home for you. Whether you're in search of a cozy cottage, a modern marvel, or a tranquil haven, this is where your house hunting adventure begins.
+
+                        Explore an extensive selection of real estate listings and uncover a world of possibilities. Our user-friendly platform makes it easy to navigate through the available houses, filter the options to match your preferences, and find the ideal property that meets your unique needs..</p>
+
                 </div>
 
             </div>
@@ -47,17 +49,53 @@
         </div>
         <div class="slant" style="background-image: url('../Public/Landing/images/slant.svg');"></div>
     </div>
-
     <div class="features-section">
         <div class="container">
             <div class="row justify-content-center text-center">
                 <div class="section-title col-lg-7" data-aos="fade-up" data-aos-delay="0">
-                    <strong class="subtitle d-block">House Features</strong>
                     <h2 class="heading font-weight-bold">Top House</h2>
-
                 </div>
             </div>
-            <div class="row align-items-stretch">
+            <form>
+                <div class="mb-4">
+
+                    <h3 class="heading font-weight-bold">Filter</h3>
+                    <div class="row">
+                        <div class="form-group col-md-4 col-lg-4 col-sm-12">
+                            <label for="exampleFormControlSelect1">House Type</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>Cottage</option>
+                                <option>Modern Home</option>
+                                <option>Haven</option>
+                                <option>Sanctuary</option>
+
+                            </select>
+                        </div>
+                        <div class="form-group col-md-4 col-lg-4 col-sm-12">
+                            <label for="exampleFormControlSelect1">House location</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>Nairobi</option>
+                                <option>Kisumu</option>
+                                <option>Machakos</option>
+
+                            </select>
+                        </div>
+                        <div class="form-group col-md-4 col-lg-4 col-sm-12">
+                            <label for="exampleFormControlSelect1">Price Range</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>1000-20000</option>
+                                <option>20001-30000</option>
+
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="d-flex align-items-end justify-content-center ">
+                        <button type="button" class="btn btn-primary">Search</button>
+                    </div>
+                </div>
+                <form>
+                <div class="row align-items-stretch">
                 <div class="col-12 col-sm-6 col-md-6 col-lg-3" data-aos="fade-up" data-aos-delay="100">
                     <div class="media-entry h-100">
                         <a href="#" style="background-image: url('../Public/Landing/images/pexels-binyamin-mellish-1396122.jpg')"></a>
@@ -129,57 +167,9 @@
             </div>
         </div>
     </div>
-    <div class="testimonial-section">
-        <div class="container">
-            <div class="row align-items-center justify-content-between">
-                <div class="col-lg-4 mb-5 section-title" data-aos="fade-up" data-aos-delay="0">
-                    <strong class="subtitle d-block">Join Now</strong>
-                    <h2 class="mb-4 font-weight-bold heading">Join More Than <strong>90,000+</strong> Amazing People Loves Our Product</h2>
-                    <p class="mb-4">House Hunt offers various marketing tools and features to showcase the listed property effectively. This includes high-quality photos, detailed descriptions, virtual tours, and interactive features, enabling house owners to present their property in the best possible light and attract more attention. </p>
-                    <p><a href="#" class="btn btn-primary">Join</a></p>
-                </div>
-                <div class="col-lg-7" data-aos="fade-up" data-aos-delay="100">
-                    <div class="testimonial--wrap">
-                        <div class="owl-single owl-carousel no-dots no-nav">
-                            
-                            <div class="testimonial-item">
-                                <div class="d-flex align-items-center mb-4">
-                                    <div class="photo mr-3">
-                                        <img src="../Public/Landing/images/pexels-nathan-cowley-1300402.jpg" alt="Image" class="img-fluid">
-                                    </div>
-                                    <div class="author">
-                                        <cite class="d-block mb-0">John Smith</cite>
-                                        <span>Owner, Sunnyville, Inc.</span>
-                                    </div>
-                                </div>
-                                <blockquote>
-                                    <p>&ldquo;Joining House Hunt was the best decision I made when selling my house&rdquo;</p>
-                                </blockquote>
-                            </div>
-                            <div class="testimonial-item">
-                                <div class="d-flex align-items-center mb-4">
-                                    <div class="photo mr-3">
-                                        <img src="../Public/Landing/images/pexels-yogendra-singh-4511649.jpg" alt="Image" class="img-fluid">
-                                    </div>
-                                    <div class="author">
-                                        <cite class="d-block mb-0">Jane Smith</cite>
-                                        <span>Owner, Greenland, Inc.</span>
-                                    </div>
-                                </div>
-                                <blockquote>
-                                    <p>&ldquo;As a house owner, I found House Hunt to be a game-changer.&rdquo;</p>
-                                </blockquote>
-                            </div>
-                        </div>
-                        <div class="custom-nav-wrap">
-                            <a href="#" class="custom-owl-prev"><span class="icon-keyboard_backspace"></span></a>
-                            <a href="#" class="custom-owl-next"><span class="icon-keyboard_backspace"></span></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+
+
 
     <div class="site-footer">
         <div class="container">
@@ -210,7 +200,7 @@
                         <div class="col-6 col-sm-6 col-md-4">
                             <div class="widget">
                                 <ul class="links list-unstyled">
-                                    <li><a href="#">Home</a></li>
+                                    <li><a href="index">Home</a></li>
                                     <li><a href="about?page=About Us">About Us</a></li>
                                 </ul>
                             </div>
@@ -256,13 +246,7 @@
     </div>
 
     <?php include('../Partial/landing/script.php'); ?>
-    <script type="text/javascript">
-        const instance = new Typewriter('#typewriter', {
-            strings: ['<p class="text-white mb-4" id="headline" data-aos="fade-up" data-aos-delay="100">Search, Giggle, and Book Your Dream House with House Hunt.Start Your Giggle-Filled House Hunt Now!</p>'],
-            autoStart: true,
-            loop: true,
-        });
-    </script>
+
 </body>
 
 </html>
