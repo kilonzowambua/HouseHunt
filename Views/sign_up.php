c1cfa222a8135f038482e0c0564aa461bbf8fb71
Anatoli <anatoli@anatoli.home>
2023-05-24 19:38:38 +0300

    Added codegen File

--- Views/sign_up.php
+++ Views/sign_up.php
@@ -101,7 +101,7 @@
             </p>
           </div>
         </div>
-        <button type="submit" class="btn mt-10 h-10 w-full bg-primary font-medium text-white hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90">
+        <button type="submit" name="sign_up" class="btn mt-10 h-10 w-full bg-primary font-medium text-white hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90">
           Sign In
         </button>
         </form>
