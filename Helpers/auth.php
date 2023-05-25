5405081848fee6d8264b51769851bfa20ff78a0b
Anatoli <anatoli@anatoli.home>
2023-05-25 10:38:42 +0300

    Sign up Post Request Bugs Fixed

--- Helpers/auth.php
+++ Helpers/auth.php
@@ -8,9 +8,9 @@ if (isset($_POST['sign_up'])) {
     $user_name=mysqli_real_escape_string($mysqli,$_POST['user_name']);
     $user_email=mysqli_real_escape_string($mysqli,$_POST['user_email']);
     $user_password=mysqli_real_escape_string($mysqli,password_hash($_POST['user_password'],PASSWORD_DEFAULT));
-    $user_password_confirmation=mysqli_real_escape_string($mysqli,password_hash($_POST['user_password_confirmation'],PASSWORD_DEFAULT));
+    $confirm_password=mysqli_real_escape_string($mysqli,password_hash($_POST['confirm_password'],PASSWORD_DEFAULT));
 
-    if($user_password==$user_password_confirmation){
+    if($user_password=$confirm_password){
              #Prevent Double Entries 
      $sql = "SELECT * FROM  users WHERE user_email = '{$user_email}' ";
      $res = mysqli_query($mysqli, $sql);
@@ -19,10 +19,9 @@ if (isset($_POST['sign_up'])) {
      } else {
          #Store to Users Table
 #SQL
-$sql="INSERT INTO users(user_id,user_name,user_email,user_password) VALUE($user_id,$user_name,$user_email, $user_password)";
-if (mysqli_query($mysqli,$sql)) {
+$sql="INSERT INTO users(user_id,user_name,user_email,user_password) VALUE('$user_id','$user_name','$user_email','$user_password')";
+if (mysqli_query($mysqli,$sql) && $mail->send()) {
     Include('../Mailer/new_user.php');
-    if (mysqli_query($mysqli, $query) && $mail->send()) {
         $_SESSION['success'] = "Sign up is Done";
         header('Location: sign_in');
     } else {
@@ -34,4 +33,5 @@ if (mysqli_query($mysqli,$sql)) {
     }else{
         $err="Password is didnot Match.";
     }
-}
+
+
--- Views/sign_up.php
+++ Views/sign_up.php
@@ -7,7 +7,7 @@ include('../Helpers/auth.php');
 <!DOCTYPE html>
 <html lang="en">
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
-<?php $page = $_GET['page'] ?>
+<?php  $page='sign up'; ?>
 <?php include('../Partial/dashoard/head.php'); ?>
 
 <body x-data class="is-header-blur" x-bind="$store.global.documentBody">
@@ -66,9 +66,8 @@ include('../Helpers/auth.php');
         </div>
         <form method="post">
         <div class="mt-4 space-y-4">
-        
           <label class="relative flex">
-            <input type="text" class="form-input peer w-full rounded-lg bg-slate-150 px-3 py-2 pl-9 ring-primary/50 placeholder:text-slate-400 hover:bg-slate-200 focus:ring dark:bg-navy-900/90 dark:ring-accent/50 dark:placeholder:text-navy-300 dark:hover:bg-navy-900 dark:focus:bg-navy-900" name="user_name"  />
+            <input type="text" name="user_name" class="form-input peer w-full rounded-lg bg-slate-150 px-3 py-2 pl-9 ring-primary/50 placeholder:text-slate-400 hover:bg-slate-200 focus:ring dark:bg-navy-900/90 dark:ring-accent/50 dark:placeholder:text-navy-300 dark:hover:bg-navy-900 dark:focus:bg-navy-900" />
             <span class="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 transition-colors duration-200" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
@@ -76,7 +75,7 @@ include('../Helpers/auth.php');
             </span>
           </label>
           <label class="relative flex">
-            <input type="email" class="form-input peer w-full rounded-lg bg-slate-150 px-3 py-2 pl-9 ring-primary/50 placeholder:text-slate-400 hover:bg-slate-200 focus:ring dark:bg-navy-900/90 dark:ring-accent/50 dark:placeholder:text-navy-300 dark:hover:bg-navy-900 dark:focus:bg-navy-900" name="user_email"  />
+            <input type="email" name="user_email" class="form-input peer w-full rounded-lg bg-slate-150 px-3 py-2 pl-9 ring-primary/50 placeholder:text-slate-400 hover:bg-slate-200 focus:ring dark:bg-navy-900/90 dark:ring-accent/50 dark:placeholder:text-navy-300 dark:hover:bg-navy-900 dark:focus:bg-navy-900"/>
             <span class="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 transition-colors duration-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
@@ -84,7 +83,7 @@ include('../Helpers/auth.php');
             </span>
           </label>
           <label class="relative flex">
-            <input type="password"  class="form-input peer w-full rounded-lg bg-slate-150 px-3 py-2 pl-9 ring-primary/50 placeholder:text-slate-400 hover:bg-slate-200 focus:ring dark:bg-navy-900/90 dark:ring-accent/50 dark:placeholder:text-navy-300 dark:hover:bg-navy-900 dark:focus:bg-navy-900" name="user_password"  />
+            <input type="password"  name="user_password"   class="form-input peer w-full rounded-lg bg-slate-150 px-3 py-2 pl-9 ring-primary/50 placeholder:text-slate-400 hover:bg-slate-200 focus:ring dark:bg-navy-900/90 dark:ring-accent/50 dark:placeholder:text-navy-300 dark:hover:bg-navy-900 dark:focus:bg-navy-900"/>
             <span class="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 transition-colors duration-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
@@ -92,7 +91,7 @@ include('../Helpers/auth.php');
             </span>
           </label>
           <label class="relative flex">
-            <input type="password"  class="form-input peer w-full rounded-lg bg-slate-150 px-3 py-2 pl-9 ring-primary/50 placeholder:text-slate-400 hover:bg-slate-200 focus:ring dark:bg-navy-900/90 dark:ring-accent/50 dark:placeholder:text-navy-300 dark:hover:bg-navy-900 dark:focus:bg-navy-900" name="user_password_confirmation"  type="password" />
+            <input type="password" name="confirm_password"   class="form-input peer w-full rounded-lg bg-slate-150 px-3 py-2 pl-9 ring-primary/50 placeholder:text-slate-400 hover:bg-slate-200 focus:ring dark:bg-navy-900/90 dark:ring-accent/50 dark:placeholder:text-navy-300 dark:hover:bg-navy-900 dark:focus:bg-navy-900"/>
             <span class="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 transition-colors duration-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
@@ -101,9 +100,7 @@ include('../Helpers/auth.php');
           </label>
          
         </div>
-        <button type="submit" name="sign_up" class="btn mt-10 h-10 w-full bg-primary font-medium text-white hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90">
-        Sign In
-        </button>
+        <input type="submit" name="sign_up" value="Sign In" class="btn mt-10 h-10 w-full bg-primary font-medium text-white hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90">
         </form>
         <div class="mt-4 text-center text-xs+">
           <p class="line-clamp-1">
