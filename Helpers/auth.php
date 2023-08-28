8d2b05f74eeb411e89a6b4787299246c6a3889b3
Anatoli <anatoli@anatoli.home>
2023-08-28 07:09:25 +0300

    Onboarding Page Completed

--- Helpers/auth.php
+++ Helpers/auth.php
@@ -4,40 +4,34 @@
 
 if (isset($_POST['sign_up'])) {
     // Declare and initialize variables
-    $user_id = mysqli_real_escape_string($mysqli, $ID);
-    $user_name = mysqli_real_escape_string($mysqli, $_POST['user_name']);
-    $user_email = mysqli_real_escape_string($mysqli, $_POST['user_email']);
-    $user_password = mysqli_real_escape_string($mysqli, password_hash($_POST['user_password'], PASSWORD_DEFAULT));
-    $confirm_password = mysqli_real_escape_string($mysqli, password_hash($_POST['confirm_password'], PASSWORD_DEFAULT));
-    $err = "";
+$user_id = mysqli_real_escape_string($mysqli, $ID);
+$user_name = mysqli_real_escape_string( $mysqli, $_POST['user_name']);
+$user_email = mysqli_real_escape_string($mysqli, $_POST['user_email']);
+$user_password = $_POST['user_password'];
+$confirm_password = $_POST['confirm_password'];
+$user_passwordhash = mysqli_real_escape_string($mysqli,password_hash($user_password,PASSWORD_DEFAULT));
 
-    if ($user_password == $confirm_password) {
-        // Check if user already exists
-        $sql = "SELECT * FROM users WHERE user_email = '$user_email'";
-        $result = mysqli_query($mysqli, $sql);
+$stmt = $mysqli->prepare("CALL SignUpUser(?, ?, ?, ?, ?, ?, @errorMessage)");
+$stmt->bind_param("ssssss", $user_id, $user_name, $user_email, $user_password, $confirm_password,$user_passwordhash);
+$stmt->execute();
 
-        if (mysqli_num_rows($result) > 0) {
-            $err = "User name already exists";
-        } else {
+$selectErr = $mysqli->query('SELECT @errorMessage')->fetch_assoc();
+$errorMessage = $selectErr['@errorMessage'];
+
+if (empty($errorMessage)) {
             // Store user data and send onboarding email
-            include('../Mailers/onboarding_mail.php');
+           // include('../Mailers/onboarding_mail.php');
 
-            $sql = "INSERT INTO users (user_id, user_name, user_email, user_password) VALUES ('$user_id', '$user_name', '$user_email', '$user_password')";
-
-            if (mysqli_query($mysqli, $sql) && $mail->send()) {
-                $_SESSION['success'] = "Sign up is Done";
-                header('Location: sign_in.php');
-                exit();
-            } else {
-                $err = "Failed! Please try again.";
-            }
-        }
-    } else {
-        $err = "Password does not match.";
-    }
-
-    
+            $_SESSION['success'] = "Sign up is Done";
+    header('Location: sign_in.php?page=Sign Up');
+    exit();
+} else {
+    $err = $errorMessage;
 }
+$stmt->close();
+
+}
+
 
 
 #Login Normal Way
@@ -58,6 +52,7 @@ if (isset($_POST['sign_in'])) {
             // User type check
             if ($user['user_type'] == '') {
                 $_SESSION['success'] = "Set Up Account";
+                $_SESSION['user_id'] = $user['user_id'];
                 header('Location: onboarding_user');
                 exit();
             } else {
@@ -71,4 +66,33 @@ if (isset($_POST['sign_in'])) {
     } else {
         $err = "Failed! Please try again.";
     }
+}
+
+
+#Onboarding settings
+if (isset($_POST['setup'])) {
+    // Declare and initialize variables
+    $user_id = mysqli_real_escape_string($mysqli, $_POST['user_id']);
+    $user_phone_no = mysqli_real_escape_string($mysqli, $_POST['user_phone_no']);
+    $user_location = mysqli_real_escape_string($mysqli, $_POST['user_location']);
+    $user_type = mysqli_real_escape_string($mysqli, $_POST['user_type']);
+
+    // Update the info
+$stmt = $mysqli->prepare("CALL UpdateUserAccount(?, ?, ?, ?,@errorMessage)");
+$stmt->bind_param("ssss", $user_id, $user_phone_no, $user_location, $user_type);
+$stmt->execute();
+
+$selectErr = $mysqli->query('SELECT @errorMessage')->fetch_assoc();
+$errorMessage = $selectErr['@errorMessage']; 
+  
+        if(!empty($errorMessage)){
+
+                $_SESSION['success'] = $errorMessage;
+                header('Location: dashboard');
+                exit();
+            
+        } else {
+            $err = "Failed! Please try again.";
+        }
+    
 }
