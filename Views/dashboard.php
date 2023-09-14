ee3cefbfec6c9dacb31a160696e5bc81fb4a952b
Anatoli <anatoli@anatoli.home>
2023-09-14 12:18:17 +0300

    Implement Get user Details via Stored procedure

--- Views/dashboard.php
+++ Views/dashboard.php
@@ -1,15 +1,38 @@
 <?php
 session_start();
-$user_id = $_SESSION['user_id'];
+
 include('../Config/codeGen.php');
 include('../Config/config.php');
 include('../Helpers/auth.php');
+$userID = mysqli_real_escape_string($mysqli,$_SESSION['user_id']);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
-<?php include('../Partial/dashoard/head.php'); ?>
+<?php $page="Dashboard"; ?>
+<?php include('../Partial/dashoard/head.php'); 
+
+#Get Current Loged in  User
+
+$query = "CALL GetUserByID($userID)";
+$result = mysqli_query($mysqli, $query);
+
+if ($result) {
+    // Fetch the user data
+    $user = mysqli_fetch_object($result);
+    if ($user) {
+      
+      $user_name=$user->user_name;
+      $user_type=$user->user_type;
+      
+      #Make Global variable
+      Global $user_name;
+      Global $user_type;
+
+}}
+?>
 
 <body x-data class="is-header-blur" x-bind="$store.global.documentBody">
   <!-- App preloader-->
@@ -61,13 +84,7 @@ include('../Helpers/auth.php');
 
           <!-- Bottom Links -->
           <div class="flex flex-col items-center space-y-3 py-3">
-            <!-- Settings -->
-            <a href="form-layout-5.html" class="flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25">
-              <svg class="h-7 w-7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-opacity="0.3" fill="currentColor" d="M2 12.947v-1.771c0-1.047.85-1.913 1.899-1.913 1.81 0 2.549-1.288 1.64-2.868a1.919 1.919 0 0 1 .699-2.607l1.729-.996c.79-.474 1.81-.192 2.279.603l.11.192c.9 1.58 2.379 1.58 3.288 0l.11-.192c.47-.795 1.49-1.077 2.279-.603l1.73.996a1.92 1.92 0 0 1 .699 2.607c-.91 1.58-.17 2.868 1.639 2.868 1.04 0 1.899.856 1.899 1.912v1.772c0 1.047-.85 1.912-1.9 1.912-1.808 0-2.548 1.288-1.638 2.869.52.915.21 2.083-.7 2.606l-1.729.997c-.79.473-1.81.191-2.279-.604l-.11-.191c-.9-1.58-2.379-1.58-3.288 0l-.11.19c-.47.796-1.49 1.078-2.279.605l-1.73-.997a1.919 1.919 0 0 1-.699-2.606c.91-1.58.17-2.869-1.639-2.869A1.911 1.911 0 0 1 2 12.947Z" />
-                <path fill="currentColor" d="M11.995 15.332c1.794 0 3.248-1.464 3.248-3.27 0-1.807-1.454-3.272-3.248-3.272-1.794 0-3.248 1.465-3.248 3.271 0 1.807 1.454 3.271 3.248 3.271Z" />
-              </svg>
-            </a>
+          
 
             <!-- Profile -->
             <div x-data="usePopper({placement:'right-end',offset:12})" @click.outside="isShowPopper && (isShowPopper = false)" class="flex">
@@ -80,14 +97,14 @@ include('../Helpers/auth.php');
                 <div class="popper-box w-64 rounded-lg border border-slate-150 bg-white shadow-soft dark:border-navy-600 dark:bg-navy-700">
                   <div class="flex items-center space-x-4 rounded-t-lg bg-slate-100 py-5 px-4 dark:bg-navy-800">
                     <div class="avatar h-14 w-14">
-                      <img class="rounded-full" src="images/avatar/avatar-12.jpg" alt="avatar" />
+                      <img class="rounded-full" src="../Public/Dashboard/images/avatar/default.png" alt="avatar" />
                     </div>
                     <div>
                       <a href="#" class="text-base font-medium text-slate-700 hover:text-primary focus:text-primary dark:text-navy-100 dark:hover:text-accent-light dark:focus:text-accent-light">
-                        Travis Fuller
+                        <?php echo  $user_name ?>
                       </a>
                       <p class="text-xs text-slate-400 dark:text-navy-300">
-                        Product Designer
+                      <?php echo  $user_type ?>
                       </p>
                     </div>
                   </div>
@@ -108,38 +125,8 @@ include('../Helpers/auth.php');
                         </div>
                       </div>
                     </a>
-                    <a href="#" class="group flex items-center space-x-3 py-2 px-4 tracking-wide outline-none transition-all hover:bg-slate-100 focus:bg-slate-100 dark:hover:bg-navy-600 dark:focus:bg-navy-600">
-                      <div class="flex h-8 w-8 items-center justify-center rounded-lg bg-info text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4.5 w-4.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
-                        </svg>
-                      </div>
-
-                      <div>
-                        <h2 class="font-medium text-slate-700 transition-colors group-hover:text-primary group-focus:text-primary dark:text-navy-100 dark:group-hover:text-accent-light dark:group-focus:text-accent-light">
-                          Messages
-                        </h2>
-                        <div class="text-xs text-slate-400 line-clamp-1 dark:text-navy-300">
-                          Your messages and tasks
-                        </div>
-                      </div>
-                    </a>
-                    <a href="#" class="group flex items-center space-x-3 py-2 px-4 tracking-wide outline-none transition-all hover:bg-slate-100 focus:bg-slate-100 dark:hover:bg-navy-600 dark:focus:bg-navy-600">
-                      <div class="flex h-8 w-8 items-center justify-center rounded-lg bg-secondary text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4.5 w-4.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                        </svg>
-                      </div>
-
-                      <div>
-                        <h2 class="font-medium text-slate-700 transition-colors group-hover:text-primary group-focus:text-primary dark:text-navy-100 dark:group-hover:text-accent-light dark:group-focus:text-accent-light">
-                          Team
-                        </h2>
-                        <div class="text-xs text-slate-400 line-clamp-1 dark:text-navy-300">
-                          Your team activity
-                        </div>
-                      </div>
-                    </a>
+                    
+                    
                     <a href="#" class="group flex items-center space-x-3 py-2 px-4 tracking-wide outline-none transition-all hover:bg-slate-100 focus:bg-slate-100 dark:hover:bg-navy-600 dark:focus:bg-navy-600">
                       <div class="flex h-8 w-8 items-center justify-center rounded-lg bg-error text-white">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4.5 w-4.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
