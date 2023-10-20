f739f078c043cef42c224fee1fee0e85d5d17fcb
Anatoli <anatoli@anatoli.home>
2023-10-20 14:49:45 +0300

    Google Auth CallBack

--- Views/sign_up.php
+++ Views/sign_up.php
@@ -1,6 +1,7 @@
 <?php
 include('../Config/codeGen.php');
 include('../Config/config.php');
+include('../Config/google_auth.php');
 include('../Helpers/auth.php');
 
 ?>
@@ -49,7 +50,7 @@ include('../Helpers/auth.php');
         <div class="mt-10 flex space-x-4">
           <button class="btn w-full space-x-3 border border-slate-300 font-medium text-slate-800 hover:bg-slate-150 focus:bg-slate-150 active:bg-slate-150/80 dark:border-navy-450 dark:text-navy-50 dark:hover:bg-navy-500 dark:focus:bg-navy-500 dark:active:bg-navy-500/90">
             <img class="h-5.5 w-5.5" src="../Public/Dashboard/images/google.svg" alt="logo" />
-            <span>Google</span>
+            <a href="<?php echo $google_client->createAuthUrl(); ?>">  <span>Google</span></a>
           </button>
          
         </div>
