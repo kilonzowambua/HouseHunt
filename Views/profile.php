ce6130c99ae1b70391d1ebfc8e6d1d9b0f6cba23
Anatoli <anatoli@anatoli.home>
2023-10-23 17:11:56 +0300

    Password management Frontend Implemented

--- Views/profile.php
+++ Views/profile.php
@@ -89,28 +89,26 @@ if ($user = mysqli_fetch_object($results)) {
               </div>
               <ul class="mt-6 space-y-1.5 font-inter font-medium">
                 <li>
-                  <a class="flex items-center space-x-2 rounded-lg bg-primary px-4 py-2.5 tracking-wide text-white outline-none transition-all dark:bg-accent" href="#">
+                  <a id="account-link" class="group flex space-x-2 rounded-lg px-4 py-2.5 tracking-wide outline-none transition-all hover:bg-slate-100 hover:text-slate-800 focus:bg-slate-100 focus:text-slate-800 dark:hover:bg-navy-600 dark:hover:text-navy-100 dark:focus:bg-navy-600 dark:focus:text-navy-100" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                      <!-- Add your SVG content here -->
                     </svg>
                     <span>Account</span>
                   </a>
                 </li>
 
                 <li>
-                  <a class="group flex space-x-2 rounded-lg px-4 py-2.5 tracking-wide outline-none transition-all hover:bg-slate-100 hover:text-slate-800 focus:bg-slate-100 focus:text-slate-800 dark:hover:bg-navy-600 dark:hover:text-navy-100 dark:focus:bg-navy-600 dark:focus:text-navy-100" href="#">
+                  <a id="password-link" class="group flex space-x-2 rounded-lg px-4 py-2.5 tracking-wide outline-none transition-all hover:bg-slate-100 hover:text-slate-800 focus:bg-slate-100 focus:text-slate-800 dark:hover:bg-navy-600 dark:hover:text-navy-100 dark:focus:bg-navy-600 dark:focus:text-navy-100" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-slate-400 transition-colors group-hover:text-slate-500 group-focus:text-slate-500 dark:text-navy-300 dark:group-hover:text-navy-200 dark:group-focus:text-navy-200" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path>
+                      <!-- Add your SVG content here -->
                     </svg>
                     <span>Password Management</span>
                   </a>
                 </li>
-
-
               </ul>
             </div>
           </div>
-          <div class="col-span-12 lg:col-span-8">
+          <div class="col-span-12 lg:col-span-8" id="account">
             <div class="card">
               <form method="post">
                 <div class="flex flex-col items-center space-y-4 border-b border-slate-200 p-4 dark:border-navy-500 sm:flex-row sm:justify-between sm:space-y-0 sm:px-5">
@@ -118,7 +116,7 @@ if ($user = mysqli_fetch_object($results)) {
                     Account Setting
                   </h2>
                   <div class="flex justify-center space-x-2">
-                    
+
                     <button type="submit" name="edit_user" class="btn min-w-[7rem] rounded-full bg-primary font-medium text-white hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90">
                       Save
                     </button>
@@ -180,16 +178,85 @@ if ($user = mysqli_fetch_object($results)) {
             </div>
 
           </div>
+          <div class="col-span-12 lg:col-span-8 hidden" id="password">
+          <div class="card">
+              <form method="post">
+                <div class="flex flex-col items-center space-y-4 border-b border-slate-200 p-4 dark:border-navy-500 sm:flex-row sm:justify-between sm:space-y-0 sm:px-5">
+                  <h2 class="text-lg font-medium tracking-wide text-slate-700 dark:text-navy-100">
+                    Password Setting
+                  </h2>
+                  <div class="flex justify-center space-x-2">
+
+                    <button type="submit" name="password_user" class="btn min-w-[7rem] rounded-full bg-primary font-medium text-white hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90">
+                      Change
+                    </button>
+                  </div>
+                </div>
+                <div class="p-4 sm:p-5">
+                  
+                  <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                    <label class="block">
+                      <span>New password</span>
+                      <span class="relative mt-1.5 flex">
+                        <input class="form-input peer w-full rounded-full border border-slate-300 bg-transparent px-3 py-2 pl-9 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent" name="user_id" value="<?php echo $user_id ?>" type="hidden">
+                        <input class="form-input peer w-full rounded-full border border-slate-300 bg-transparent px-3 py-2 pl-9 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent" name="user_password"  type="password">
+                        <span class="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
+                          
+                        </span>
+                      </span>
+                    </label>
+                    <label class="block">
+                      <span>Confirm Pasword </span>
+                      <span class="relative mt-1.5 flex">
+                        <input class="form-input peer w-full rounded-full border border-slate-300 bg-transparent px-3 py-2 pl-9 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent" name="user_confirm_password"  type="password">
+                        <span class="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
+                         
+                        </span>
+                      </span>
+                    </label>
+                   
+
+
+                  </div>
+                </div>
+              </form>
+            </div>
+    </div>
         </div>
       </main>
     </div>
 
     <div id="x-teleport-target"></div>
     <?php include('../Partial/dashoard/script.php'); ?>
-   
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            const accountLink = document.getElementById("account-link");
+            const passwordLink = document.getElementById("password-link");
+            const accountDiv = document.getElementById("account");
+            const passwordDiv = document.getElementById("password");
+
+            accountLink.classList.add("bg-primary");
+
+            accountLink.addEventListener("click", function (event) {
+                event.preventDefault();
+                accountDiv.classList.remove("hidden");
+                passwordDiv.classList.add("hidden");
+                accountLink.classList.add("bg-primary");
+                passwordLink.classList.remove("bg-primary");
+            });
+
+            passwordLink.addEventListener("click", function (event) {
+                event.preventDefault();
+                passwordDiv.classList.remove("hidden");
+                accountDiv.classList.add("hidden");
+                passwordLink.classList.add("bg-primary");
+                accountLink.classList.remove("bg-primary");
+            });
+        });
+    </script>
 
 
-    <?php } ?>
+  <?php } ?>
   </body>
 
 
